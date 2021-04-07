6c7807dfcda5f7d563b59344e7c1aba94471bd7b
jeewon94 <jayyy1224@gmail.com>
2021-04-07 18:28:41 +0900

    04/07 nugu 섹션 만드는 중 + 프포덕트 섹션 미완성

--- js/custom.js
+++ js/custom.js
@@ -5,20 +5,63 @@
 
 $(function(){
 
-  var swiper = new Swiper('.swiper-container', {
-    centeredSlides: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // var swiper = new Swiper('.swiper-container', {
+  //   centeredSlides: true,
+  //   cssWidthAndHeight: true,
+
+  //   slidesPerView: 'auto',
+  //   spaceBetween: 30,
+  //   loop: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // });
   
+
+
+      
+  var mySwiper = new Swiper ('.swiper-container', {
+    speed: 400,
+    spaceBetween: 300,
+    initialSlide: 0,
+    //truewrapper adoptsheight of active slide
+    autoHeight: false,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // delay between transitions in ms
+    autoplay: 5000,
+    autoplayStopOnLast: false, // loop false also
+    // If we need pagination
+    // pagination: '.swiper-pagination',
+    // paginationType: "bullets",
+    pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    
+    // And if we need scrollbar
+    //scrollbar: '.swiper-scrollbar',
+    // "slide", "fade", "cube", "coverflow" or "flip"
+    effect: 'slide',
+    // Distance between slides in px.
+    spaceBetween: 220,
+    //
+    slidesPerView: 2,
+    //
+    centeredSlides: true,
+    //
+    slidesOffsetBefore: 0,
+    //
+    grabCursor: true,
+  })        
 
 })
