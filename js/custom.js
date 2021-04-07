f15d8a57b764da958552ba7abd1383ca88bad804
jeewon94 <jayyy1224@gmail.com>
2021-04-07 14:03:55 +0900

    04/07 (수) 프로덕트 섹션 만드는 중

--- js/custom.js
+++ js/custom.js
@@ -4,31 +4,21 @@
 
 
 $(function(){
-  new Swiper('.swiper-container', {
-    pagination : { // 페이징 설정
-      el : '.swiper-pagination',
-      clickable : true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
-    },
-    navigation : { // 네비게이션 설정
-      nextEl : '.swiper-button-next', // 다음 버튼 클래스명
-      prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
-    },
-    // slidesPerView: 1.1,
+
+  var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
-    slidesPerView: 2,
-        loop: true,
-    spaceBetween: 220,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
   });
   
-
-
-
-
-
-
 
 })
