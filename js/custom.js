0008bacd1011cfeba150143ff577b59fa2e09680
jeewon94 <jayyy1224@gmail.com>
2021-04-08 18:16:25 +0900

    04/08 (목) 뉴앨범 섹션 제작 중

--- js/custom.js
+++ js/custom.js
@@ -5,63 +5,101 @@
 
 $(function(){
 
-  // var swiper = new Swiper('.swiper-container', {
-  //   centeredSlides: true,
-  //   cssWidthAndHeight: true,
+  var swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    cssWidthAndHeight: true,
 
-  //   slidesPerView: 'auto',
-  //   spaceBetween: 30,
-  //   loop: true,
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     clickable: true,
-  //   },
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  // });
-  
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-
-      
-  var mySwiper = new Swiper ('.swiper-container', {
-    speed: 400,
+        speed: 400,
     spaceBetween: 300,
     initialSlide: 0,
     //truewrapper adoptsheight of active slide
     autoHeight: false,
     // Optional parameters
-    direction: 'horizontal',
+    // direction: 'horizontal',
     loop: true,
     // delay between transitions in ms
     autoplay: 5000,
-    autoplayStopOnLast: false, // loop false also
-    // If we need pagination
-    // pagination: '.swiper-pagination',
-    // paginationType: "bullets",
-    pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-    // Navigation arrows
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    
-    // And if we need scrollbar
-    //scrollbar: '.swiper-scrollbar',
-    // "slide", "fade", "cube", "coverflow" or "flip"
-    effect: 'slide',
-    // Distance between slides in px.
     spaceBetween: 220,
-    //
-    slidesPerView: 2,
-    //
-    centeredSlides: true,
-    //
-    slidesOffsetBefore: 0,
-    //
-    grabCursor: true,
-  })        
+      //
+      slidesPerView: 2,
+      //
+      centeredSlides: true,
+      //
+      slidesOffsetBefore: 0,
+      //
+      grabCursor: true,
+  });
+  
+
+
+      
+  // var mySwiper = new Swiper ('.swiper-container', {
+  //   speed: 400,
+  //   spaceBetween: 300,
+  //   initialSlide: 0,
+  //   //truewrapper adoptsheight of active slide
+  //   autoHeight: false,
+  //   // Optional parameters
+  //   // direction: 'horizontal',
+  //   loop: true,
+  //   // delay between transitions in ms
+  //   autoplay: 5000,
+  //   autoplayStopOnLast: false, // loop false also
+  //   // If we need pagination
+  //   // pagination: '.swiper-pagination',
+  //   // paginationType: "bullets",
+  //   pagination: {
+  //         el: '.swiper-pagination',
+  //         clickable: true,
+  //       },
+  //   // Navigation arrows
+  //   nextButton: '.swiper-button-next',
+  //   prevButton: '.swiper-button-prev',
+    
+  //   // And if we need scrollbar
+  //   //scrollbar: '.swiper-scrollbar',
+  //   // "slide", "fade", "cube", "coverflow" or "flip"
+  //   effect: 'slide',
+  //   // Distance between slides in px.
+  //   spaceBetween: 220,
+  //   //
+  //   slidesPerView: 2,
+  //   //
+  //   centeredSlides: true,
+  //   //
+  //   slidesOffsetBefore: 0,
+  //   //
+  //   grabCursor: true,
+  // })        
+
+
+
+  $(".pro_item").hide();
+
+  $(".pro_tab > ul > li").mouseover(function(){
+    var index = $(this).index();
+    $(".pro_item").hide();
+    $(".pro_item").eq(index).show();
+  })
+
+  $(".pro_tab > ul > li").eq(0).trigger("click");
+  $(".pro_item").eq(0).show();
+
+
+
+
+
 
 })
